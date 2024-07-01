0acf2be54ab7ab373799a3fc4c0a80dad7a2caaa
Pratiksha Bagade <66509396+Pratiksha1719@users.noreply.github.com>
2024-07-01 13:17:51 +0530

    Update hourglass.php

--- hourglass.php
+++ hourglass.php
@@ -9,12 +9,14 @@
 
 */
 
-for ($i = 0; $i < 5; $i++) {
-    for ($j = 0; $j < 5; $j++) {
-        if ($i == 0 || $i == 4) {
+$row = 7;
+$col = 7;
+for ($i = 0; $i < $row; $i++) {
+    for ($j = 0; $j < $col; $j++) {
+        if ($i == 0 || $i == $row-1) {
             echo " * ";
         } else {
-            if ($j == $i || $j + $i == 4) {
+            if ($j == $i || $j + $i == $row-1) {
                 echo " * ";
             } else {
                 echo "   ";
